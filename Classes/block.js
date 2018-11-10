0395caeba7682965ab2060959c46ab6393aaba2c
Jhemeson Silva Mota <jhemesonmotta@gmail.com>
2018-11-10 10:46:11 -0300

    Adicionando mineração

--- Classes/block.js
+++ Classes/block.js
@@ -1,20 +1,42 @@
 const sha256 = require('crypto-js/sha256')
 
 class Block {
-    constructor(index = 0, previousHash = null, data = 'Genesis block') {
+    constructor(index = 0, previousHash = null, data = 'Genesis block', difficulty = 1) {
         this.index = index
         this.previousHash = previousHash
         this.data = data
         this.timestamp = new Date()
-        this.hash = this.generateHash()
+
+        //dificuldade geral do sistema
+        this.difficulty = difficulty
+
+        //quantidade de tentativas até que o hash correto seja criado
+        this.nonce = 0
+        
+        this.mine()
     }
 
+    // função que gerará hashes
     generateHash() {
-        return sha256(this.index
+        return sha256(
+            this.index
             + this.previousHash
             + JSON.stringify(this.data)
-            + this.timestamp)
-        .toString()
+            + this.timestamp
+            + this.nonce
+        ).toString()
+    }
+
+    // A função mine vai criar hashes até que a quantidade de zeros à esquerda do hash seja atentida.
+    mine() {
+        this.hash = this.generateHash()
+    
+        while (!(/^0*$/.test(
+                this.hash.substring(0, this.difficulty)
+            ))) {
+                this.nonce++
+                this.hash = this.generateHash()
+        }
     }
 }
 
